12102c1e590e0fc45dd220dc179cc54fecfc9dc8
fiyk <fiyk@fiyk.com>
2019-02-04 01:50:28 +0300

    v3

--- app.js
+++ app.js
@@ -1,114 +1,44 @@
 (function () {
     'use strict';
 
-    var shoppingList = [
-        {
-            name: "Milk",
-            quantity: "34"
-        },
-        {
-            name: "Donuts",
-            quantity: "65"
-        },
-        {
-            name: "Cookies",
-            quantity: "25"
-        },
-        {
-            name: "Chocolate",
-            quantity: "54"
-        },
-        {
-            name: "Drink",
-            quantity: "10"
-        }
-    ];
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController ', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems);
 
-    angular.module('ShoppingListCheckOff', [])
-        .controller('ToBuyController', ToBuyController)
-        .controller('AlreadyBoughtController', AlreadyBoughtController)
-        .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var nidCtrl = this;
 
-    ToBuyController.$inject = ['ShoppingListCheckOffService'];
-    function ToBuyController(ShoppingListCheckOffService) {
-        var tobuy = this;
+        nidCtrl.found = "";
 
-        tobuy.itemName = "";
-        tobuy.itemQuantity = "";
-        tobuy.items = ShoppingListCheckOffService.getItemsToBuy();
-
-        tobuy.addItem = function () {
-            ShoppingListCheckOffService.addItemToBuy(tobuy.itemName, tobuy.itemQuantity);
-        };
-
-        tobuy.removeItem = function (itemIdex) {
-            ShoppingListCheckOffService.removeItemToBuy(itemIndex);
-        };
-
-        tobuy.buyToBought = function (itemIndex) {
-            ShoppingListCheckOffService.addItemAlreadyBought(tobuy.items[itemIndex].name, tobuy.items[itemIndex].quantity);
-            ShoppingListCheckOffService.removeItemToBuy(itemIndex);
-        }
-    }
-
-    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-    function AlreadyBoughtController(ShoppingListCheckOffService) {
-        var alreadybought = this;
-
-        alreadybought.items = ShoppingListCheckOffService.getItemsAlreadyBought();
-        alreadybought.itemName = "";
-        alreadybought.itemQuantity = "";
-        alreadybought.addItem = function () {
-            ShoppingListCheckOffService.addItemAlreadyBought(alreadybought.itemName, alreadybought.itemQuantity);
-        };
-
-        alreadybought.removeItem = function (itemIdex) {
-            ShoppingListCheckOffService.removeItemAlreadyBought(itemIndex);
+        nidCtrl.found = function () {
+            MenuSearchService.getMatchedMenuItems(nidCtrl.searchTerm);
         };
     }
 
-
-    function ShoppingListCheckOffService() {
+    function MenuSearchService() {
         var service = this;
 
-        // List of items to buy
-        var itemsToBuy = shoppingList;
+        var foundItems = [];
 
-        // List of items already bought
-        var itemsAlreadyBought = []
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http('https://davids-restaurant.herokuapp.com/menu_items.json').then(function (result) {
+                // process result and only keep items that match
+                foundItems.push(result);
 
-        service.addItemToBuy = function (itemName, quantity) {
-            var item = {
-                name: itemName,
-                quantity: quantity
-            };
-            itemsToBuy.push(item);
+                // return processed items
+                return foundItems;
+            });
         };
 
-        service.addItemAlreadyBought = function (itemName, quantity) {
-            var item = {
-                name: itemName,
-                quantity: quantity
-            };
-            itemsAlreadyBought.push(item);
+        service.removefoundItems = function (itemIdex) {
+            foundItems.splice(itemIdex, 1);
         };
 
-        service.removeItemToBuy = function (itemIdex) {
-            itemsToBuy.splice(itemIdex, 1);
+        service.getfoundItems = function () {
+            return foundItems;
         };
-
-        service.removeItemAlreadyBought = function (itemIdex) {
-            itemsAlreadyBought.splice(itemIdex, 1);
-        };
-
-        service.getItemsToBuy = function () {
-            return itemsToBuy;
-        };
-
-        service.getItemsAlreadyBought = function () {
-            return itemsAlreadyBought;
-        }
-
     }
 
 })();
